581fd55b15cac59dfda19536156d090b383606ca
solar <solar@michaels.com>
2023-03-08 15:41:37 +0800

    1

---
+++ clearCart.js
@@ -0,0 +1,21 @@
+// clear cart in /cart
+var clearCaratTimer = setInterval(function() {
+    if (location.href.indexOf('/cart') > -1 && __NEXT_DATA__ && typeof __NEXT_DATA__ === 'object') {
+        clearInterval(clearCaratTimer)
+        setTimeout(function() {
+            var headerBox = document.querySelector('.headerBox p')
+            if (headerBox && headerBox.textContent !== '(0 item)') {
+                var monitor = setInterval(function() {
+                    var headerBoxMonitor = document.querySelector('.headerBox p')
+                    if (!headerBoxMonitor || (headerBoxMonitor && headerBoxMonitor.textContent === '(0 item)')) {
+                        clearInterval(monitor)
+                        dataLayer.push({
+                            event: 'clearCart',
+                            time: new Date().getTime(),
+                        });
+                    }
+                }, 1000)
+            }
+        }, 1000)
+    }
+}, 500)
